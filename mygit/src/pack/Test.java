653f447dc1320cb7fd0463d884512d65508466da
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2014-10-31 16:31:25 +0530

    Project created

---
+++ mygit/src/pack/Test.java
@@ -0,0 +1,17 @@
+package pack;
+
+import javax.swing.JFrame;
+
+public class Test{
+
+	public static void main(String[] args) {
+
+		JFrame fr = new JFrame();
+		
+		fr.setSize(500,500);
+		fr.setVisible(true);
+		fr.setLocation(500, 500);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
